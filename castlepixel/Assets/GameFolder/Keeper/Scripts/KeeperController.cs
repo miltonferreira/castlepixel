1e8e94d0d1ed48f3b137e03a66d9c033341e4b54
miltonferreira <53456302+miltonferreira@users.noreply.github.com>
2022-12-31 22:21:36 -0300

    Inimigo Keeper Adicionado

---
+++ castlepixel/Assets/GameFolder/Keeper/Scripts/KeeperController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeeperController : MonoBehaviour
+{
+
+    public Transform skin;
+    public Transform keeperRange;
+
+    public Transform a;
+    public Transform b;
+
+    public bool goRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // desativa este script --------------------------------------------------------------------
+        if(GetComponent<Character>().life <= 0){
+            keeperRange.GetComponent<CircleCollider2D>().enabled = false;    // desativa colisor de range
+            GetComponent<CapsuleCollider2D>().enabled = false;      // desativa colisor do keeper
+            this.enabled = false;            
+        }
+
+        if(skin.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("KeeperAttack")){
+            return;
+        }
+
+        if(goRight){
+            skin.localScale = new Vector3(1f,1f,1f);
+            if(Vector2.Distance(transform.position, b.position) < 0.1f){
+                goRight = false;    // inverte para esquerda
+            }
+            transform.position = Vector2.MoveTowards(transform.position, b.position, 0.2f * Time.deltaTime); // vai para direita
+        }else{
+            skin.localScale = new Vector3(-1f,1f,1f);
+            if(Vector2.Distance(transform.position, a.position) < 0.1f){
+                goRight = true;    // inverte para direita
+            }
+            transform.position = Vector2.MoveTowards(transform.position, a.position, 0.2f * Time.deltaTime); // vai para esquerda
+        }
+        
+    }
+}
